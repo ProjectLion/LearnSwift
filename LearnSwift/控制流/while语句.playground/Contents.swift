fe697fc7550ab0bd854a7e575a211743157d4456
ht <htreturntrue@163.com>
2019-07-25 14:57:43 +0800

    添加 repeat while 语句的执行顺序注释

--- LearnSwift/控制流/while语句.playground/Contents.swift
+++ LearnSwift/控制流/while语句.playground/Contents.swift
@@ -9,7 +9,7 @@ import UIKit
 // while语句
 var num = 5
 while num > 0 {     // 只要num大于0就一直会执行while代码块中的代码
-    print("执行")
+    print("执行\(num)")
     num -= 1
 }
 // 经典的死循环
@@ -19,8 +19,9 @@ while num > 0 {     // 只要num大于0就一直会执行while代码块中的代
 
 // Swift弃用了do while语句，相应的使用 repeat while 语句。
 var num1 = 5
+// 先判断while的条件，决定是否执行repeat里的语句
 repeat {
-    print("执行")
+    print("执行\(num1)")
     num1 -= 1
 } while num1 > 0
 
